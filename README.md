7e4edbbab0e438c069f9e8556c7f9ec376434955
HiteshhK <kaushikhitesh002@gmail.com>
2022-01-07 15:10:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-dnnqak
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dnnqak)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import fetchWeather from  './api/fetchWeather';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+};
+export default App;
