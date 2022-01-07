f138aeb6f41b69b365257d1584ef399ca7f26c37
HiteshhK <kaushikhitesh002@gmail.com>
2022-01-07 15:37:17 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import fetchWeather from  './api/fetchWeather';
+import { fetchWeather } from './api/fetchWeather';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query);
+    }
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
   );
 };
